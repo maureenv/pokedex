f6ab21a4acff283e153dc246429e8061262571c7
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-11-27 14:12:15 -0500

    Added route and view

---
+++ app/app.js
@@ -0,0 +1,17 @@
+(function() {
+  var app = angular.module('pokemonApp', ['ngRoute']);
+    // $routeProvider is used to configure the routes. We use the ngRoute config() to configure the $routeProvider. The config() takes a function which takes the $routeProvide as a parameter and the routing configuration goes inside the function
+
+    // $routeProvider has a simple API, accepting either the when() or otherwise() method.
+  app.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+      controller: 'PokemonController', // since controller is being passed into template through the route I no longer have to attach controller in template itself
+      templateUrl: 'app/views/pokemon.html' // to see if page is working just create a route and a view for it. Don't need controller yet
+
+    })
+
+  })
+
+
+}());
